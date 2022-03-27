a145e682997643c4d4d40509b724f6177b2c69d2
RafaelPilartes <rafaelpilartes.rlps@gmail.com>
2022-03-27 05:05:50 +0100

    first commit

---
+++ js/tradutor.js
@@ -0,0 +1,6 @@
+let btnSelectLang = document.getElementsByClassName('.logo');
+let ulSelect = document.querySelector('.language-selected ul');
+
+btnSelectLang.addEventListener("click", () => {
+  console.log('ola')
+});
